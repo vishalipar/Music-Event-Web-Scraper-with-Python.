a2f62179a562033ab59506e1bf4c0609a56bf116
vishalipar <vishalipar04@gmail.com>
2024-09-12 17:39:21 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import requests
+import selectorlib
+import smtplib, ssl
+import os
+import time
+import sqlite3
+
+# SELECT * FROM events WHERE date='2088.10.15'
+
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "vishaliparce23@sndcoe.ac.in"
+    password = "mtse jetk cswq zotx"
+
+    receiver = "sampleemail811@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+    print("Email was sent!")
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    bank, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE bank=? AND city=? AND date=?", (bank, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
